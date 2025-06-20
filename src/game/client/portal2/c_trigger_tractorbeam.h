1feb76c06feb64fe50c708c29c93d0334e6838f9
Mandrew <Wonderland_War@outlook.com>
2025-06-19 23:59:32 -0500

    More tractorbeam fixes

--- src/game/client/portal2/c_trigger_tractorbeam.h
+++ src/game/client/portal2/c_trigger_tractorbeam.h
@@ -114,7 +114,9 @@ public:
 	Vector	GetForceDirection() const;
 	Vector	GetStartPoint( void ) const { return m_vStart; }
 	Vector	GetEndPoint( void ) const { return m_vEnd; }
-
+	
+	static void RecvProxy_Start( const CRecvProxyData *pData, void *pStruct, void *pOut );
+	static void RecvProxy_End( const CRecvProxyData *pData, void *pStruct, void *pOut );
 
 	//PaintBlobVector_t m_blobs;
 
